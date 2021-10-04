8b92b5222af01d7904310de5610b974e81c06bb2
Lucas Beeman <lucasbeeman12@gmail.com>
2021-10-04 09:24:44 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StaticMembers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Forest num1 = new Forest("asdf");
+            Console.WriteLine(Forest.ForestsCreated);
+            Forest num2 = new Forest("fdsa");
+            Console.WriteLine(Forest.ForestsCreated);
+        }
+    }
+}
